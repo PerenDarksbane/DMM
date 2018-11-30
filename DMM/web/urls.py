e3fcfc8e2d62fea803728f11ec26b3700ebe8437
PerenDarksbane <ajbimm@gmail.com>
2018-11-29 23:23:48 -0500

    Done Registration
    Next is login and out

--- DMM/web/urls.py
+++ DMM/web/urls.py
@@ -3,7 +3,7 @@ from django.urls import re_path, path
 from web.views import *
 
 urlpatterns = [
+    path('register', register),
     path('posts', posts),
     re_path('', index),
 ]
-
--- DMM/web/views.py
+++ DMM/web/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
+from django.template import RequestContext
+from web.models import UserForm, UserProfileForm
 
 # Create your views here.
+def register(request):
+    context = RequestContext(request)
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+    return render(
+            request,
+            'register.html',
+            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered},
+            context)
+
 def index(request):
     return render(request, 'index.html')
 
